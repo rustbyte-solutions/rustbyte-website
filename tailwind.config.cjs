98aabcfd08fb91dbc8c4de4bdc0ce137fca0aec9
Ender2K89 <ender.yeeterson@gmail.com>
2023-05-25 01:13:53 +0200

    Rewrote like 90% of the entire site, looks nice.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,21 @@ module.exports = {
 				"inter": ["Inter", ...defaultTheme.fontFamily.sans],
 			}
 		},
+		animation: {
+			text: 'text 1s ease infinite',
+		  },
+		  keyframes: {
+			text: {
+			  '0%, 100%': {
+				'background-size': '200% 200%',
+				'background-position': 'left center',
+			  },
+			  '50%': {
+				'background-size': '200% 200%',
+				'background-position': 'right center',
+			  },
+			},
+		  },
 	},
 	plugins: [],
 }
