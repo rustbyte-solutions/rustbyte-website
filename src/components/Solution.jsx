e3cf3503ec4c4f1517d350114c48c43aa5d3b74a
vague2k <ilovedrawing056@gmail.com>
2023-08-14 21:54:53 -0400

    Messed around w git cmd sorry for any weird stuff

--- src/components/Solution.jsx
+++ src/components/Solution.jsx
@@ -4,7 +4,7 @@ function Solution ({name, link, description}) {
     return (
         <a className="flex hover:-translate-y-4 duration-500" href={link} target="_blank">
             <div className="bg-[#F9F9F9] dark:bg-[#0A0A0A] border-2 border-[#C6C6C6] dark:border-[#353535] shadow-xl px-5 py-2 rounded-lg duration-500 w-56 md:w-64">
-                <img className="mt-2 rounded-xl" src={image} alt={name}/>
+                <img className="mt-2 rounded-xl" height="512px" width="512px" src={image} alt={name}/>
                 
                 <h1 className="mt-2 text-xl font-bold">
                     {name}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			
+			white: colors.white,
+			black: colors.black,
+
+			'text': {
+				dark: '#fdfdfc',
+				light: '#030302',
+			},
+			'background': {
+				darker: '#0A0A0A',
+				dark: '#0e0e0e',
+				light: '#fdfdfc',
+			},
+			'primary': '#ace128',
+			'secondary': {
+				dark: '#242522',
+				light: '#d7d8d5',
+			},
+			'accent': {
+				dark: '#8d8797',
+				light: '#756c72',
+			},
+		}
 	},
 	plugins: []
 }
