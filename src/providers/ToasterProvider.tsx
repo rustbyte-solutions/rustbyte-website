e624b4938caddbce944159ca4d93edbbab005d62
Ender2K89 <ender.yeeterson@gmail.com>
2023-08-16 22:52:53 +0200

    Minor things

--- src/providers/ToasterProvider.tsx
+++ src/providers/ToasterProvider.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "react-hot-toast"
 
 const ToasterProvider = () => {
     return (
-        <Toaster/>
+        <Toaster position="top-right"/>
     )
 }
 
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,8 +3,9 @@ import toast from "react-hot-toast";
 import { useSupabaseClient } from "../providers/SupabaseProvider";
 import axios from "axios";
 
-function Contact () {
+const defaultInputClass = "p-2 border-2 rounded-lg outline-none border-secondary-light dark:border-secondary-dark focus:border-accent-light focus:dark:border-accent-dark dark:bg-background-darker bg-white duration-500"
 
+function Contact () {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
@@ -12,7 +13,6 @@ function Contact () {
     const supabaseClient = useSupabaseClient();
 
     const onSubmit = async (event: any) => {
-
         event.preventDefault();
         
         if (!email || !message) {
@@ -34,6 +34,7 @@ function Contact () {
                 toast.error("Invalid email address!");
                 return;
             }
+
         } catch (error: any) {
             toast.error(error);
             return;
@@ -60,7 +61,6 @@ function Contact () {
     return ([
         <div id="contact"/>,
         <div className="w-[20rem] md:w-[67rem] mb-16 md:h-3/4 mx-auto md:flex justify-between mt-10 sm:mt-36">
-
             <div className="inline font-inter font-bold">
                 <h1 className="text-5xl md:text-7xl text-center tracking-tight md:text-left">Contact Us</h1>
                 <p className="font-medium text-center md:text-left mt-5 md:mt-10 text-xl md:text-2xl leading-tight">
@@ -72,13 +72,15 @@ function Contact () {
 
             <div className="w-full md:w-1/2 flex top-0 mt-8 md:mt-2">
                 <form onSubmit={onSubmit} id="rustbyte-contact" className="flex flex-col gap-2 w-full">
-                    <input onChange={(event) => setName(event.target.value)} type="text" name="name" placeholder="Name" className="p-2 border-2 border-gray-400 dark:border-[#353535] dark:bg-[#0A0A0A] bg-white rounded-lg" value={name}/>
-                    <input onChange={(event) => setEmail(event.target.value)} type="text" name="email" placeholder="Email" className="p-2 border-2 border-gray-400 dark:border-[#353535] dark:bg-[#0A0A0A] bg-white rounded-lg" value={email}/>
-                    <textarea onChange={(event) => setMessage(event.target.value)} name="message" placeholder="Leave a message!" className="h-2/3 p-2 border-2 border-gray-400 dark:border-[#353535] dark:bg-[#0A0A0A] bg-white rounded-lg" value={message}></textarea>
+                    <input onChange={(event) => setName(event.target.value)} type="text" name="name" placeholder="Name" value={name} className={defaultInputClass}/>
+                    <input onChange={(event) => setEmail(event.target.value)} type="text" name="email" placeholder="Email" value={email} className={defaultInputClass}/>
+                    <textarea onChange={(event) => setMessage(event.target.value)} name="message" placeholder="Leave a message!" value={message} className={`${defaultInputClass} h-2/3`}/>
+
                     <button type="submit" className="rounded-md md:w-1/4 px-9 py-4 font-medium bg-primary text-black">Submit</button>
                 </form>
             </div>
         </div>
     ]);
 };
+
 export default Contact;
