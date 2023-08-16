acf5828b66cbdb0f2275a422ada228f057846718
vague2k <121782036+vague2k@users.noreply.github.com>
2023-08-15 22:36:54 -0400

    Manually copy `rewrite` into `main`

---
+++ src/data/team.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        name: "Cannabis",
+        roles: "Plugin Dev",
+        rolesTitle: "Founder - Plugin Developer",
+        discordId: "97145520446787584",
+        description: "Rustbyte's main founder. Without him rustbyte and many of our plugins wouldn't exist.",
+        link: "https://discord.com/users/97145520446787584"
+    },
+    {
+        name: "Ender",
+        roles: "Frontend Dev",
+        rolesTitle: "Founder - Bot Developer - Website Developer",
+        discordId: "1022842005920940063",
+        description: "One of the main frontend devs, Ender made the website you're looking at come to life.",
+        link: "https://discord.com/users/1022842005920940063"
+    },
+    {
+        name: "Albert",
+        roles: "Designer",
+        rolesTitle: "Founder - Plugin Developer",
+        discordId: "204040618811326465",
+        description: "Designer first, frontend second. Albert helped in coding the website and is our main designer.",
+        link: "https://discord.com/users/204040618811326465"
+    },
+    {
+        name: "Andy",
+        roles: "Backend Dev",
+        rolesTitle: "Website Developer",
+        discordId: "683002779396079667",
+        description: "If there's any backend that needs handling, Andy is responsible for all behind-the-scenes action.",
+        link: "https://discord.com/users/683002779396079667"
+    }
+];
