a2ab3eed2344a21dcfdddf8074748bc7d3b1c68b
Ender2K89 <ender.yeeterson@gmail.com>
2023-08-19 21:49:23 +0200

    Revert astro compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
-import compress from "astro-compress";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rustbyte.dev",
-  integrations: [tailwind(), react(), compress(), sitemap()]
+  integrations: [tailwind(), react(), sitemap()]
 });
