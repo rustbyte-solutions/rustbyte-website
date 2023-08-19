77f839252829207b7724247374dcaf2a667f8945
Ender2K89 <ender.yeeterson@gmail.com>
2023-08-19 21:59:43 +0200

    Add compressor back, turn off HTML for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
+import compress from "astro-compress";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rustbyte.dev",
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [
+    tailwind(),
+    react(),
+    compress({
+      CSS: true,
+      HTML: false,
+      Image: true,
+      JavaScript: true,
+      SVG: true,
+    }),
+    sitemap()]
 });
