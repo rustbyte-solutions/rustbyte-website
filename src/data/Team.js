e705f558aabf414912f5c1bd6e23fcc969e34976
Ender2K89 <ender.yeeterson@gmail.com>
2023-08-31 18:44:11 +0200

    Small changes to Team.js

--- src/data/Team.js
+++ src/data/Team.js
@@ -2,29 +2,29 @@ export default [
     {
         name: "Cannabis",
         roles: "Plugin Dev",
-        rolesTitle: "Founder - Plugin Developer",
+        rolesTitle: "Founder, Plugin Developer",
         discordId: "97145520446787584",
         description: "Rustbyte's main founder. Without him rustbyte and many of our plugins wouldn't exist.",
     },
     {
         name: "Ender",
-        roles: "Frontend Dev",
-        rolesTitle: "Founder - Bot Developer - Website Developer",
+        roles: "Website Dev",
+        rolesTitle: "Founder, Website Developer, and Bot Developer",
         discordId: "1022842005920940063",
-        description: "One of the main frontend devs, Ender made the website you're looking at come to life.",
+        description: "One of the main website devs, Ender helped the website you're looking at come to life.",
     },
     {
         name: "Albert",
         roles: "Designer",
-        rolesTitle: "Founder - Plugin Developer",
+        rolesTitle: "Founder, Website Developer, and Designer",
         discordId: "204040618811326465",
         description: "Designer first, frontend second. Albert helped in coding the website and is our main designer.",
     },
     {
         name: "Andy",
-        roles: "Backend Dev",
-        rolesTitle: "Website Developer",
+        roles: "Bot Dev",
+        rolesTitle: "Bot Developer",
         discordId: "683002779396079667",
-        description: "If there's any backend that needs handling, Andy is responsible for all behind-the-scenes action.",
+        description: "Andy helps us integrate",
     }
 ];
